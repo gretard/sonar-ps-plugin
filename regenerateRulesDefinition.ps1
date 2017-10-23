c2b6950d02be4431b4212864af3239cf8071cf66
gretar <rd.greta@gmail.com>
2017-10-23 18:19:21 +0300

    Update rules regeneration script

--- regenerateRulesDefinition.ps1
+++ regenerateRulesDefinition.ps1
@@ -1,5 +1,20 @@
-$reportFile = Join-path $PSScriptRoot "powershell-rules.xml"
+	$reportFile = Join-path $PSScriptRoot "sonar-ps-plugin\src\main\resources\powershell-rules.xml"
+	$profileFile = Join-path $PSScriptRoot "sonar-ps-plugin\src\main\resources\powershell-profile.xml"
 
+	$xmlProfileWriter = New-Object System.XMl.XmlTextWriter($profileFile , $Null);
+	  # Set The Formatting
+    $xmlProfileWriter.Formatting = "Indented"
+    $xmlProfileWriter.Indentation = "4"
+    
+    # Write the XML Declaration
+    $xmlProfileWriter.WriteStartDocument();
+    
+    # Start Issues XML Element
+    $xmlProfileWriter.WriteStartElement("profile");
+	$xmlProfileWriter.WriteElementString("name", "Sonar way");
+	$xmlProfileWriter.WriteElementString("language", "ps");
+	$xmlProfileWriter.WriteStartElement("rules");
+	
     # Create The Document
     $xmlWriter = New-Object System.XMl.XmlTextWriter($reportFile , $Null);
     
@@ -14,18 +29,25 @@ $reportFile = Join-path $PSScriptRoot "powershell-rules.xml"
     $xmlWriter.WriteStartElement("psrules");
 
     $powershellRules = Get-ScriptAnalyzerRule;
-
+   
     foreach ($rule in $powershellRules) 
     { 
+	$xmlProfileWriter.WriteStartElement("rule");
+	$xmlProfileWriter.WriteElementString("key", $rule.RuleName);
+    $xmlProfileWriter.WriteElementString("repositoryKey", "ps-psanalyzer");
+	$xmlProfileWriter.WriteEndElement();
+
 
      $xmlWriter.WriteStartElement("rule");
             $xmlWriter.WriteElementString("key", $rule.RuleName)
                         $xmlWriter.WriteElementString("internalKey", $rule.RuleName)
-         $xmlWriter.WriteElementString("name", $rule.CommonName)
-         $xmlWriter.WriteElementString("description", $rule.Description)
-           $xmlWriter.WriteElementString("cardinality", "SINGLE")
-            $xmlWriter.WriteElementString("remediationFunction", "LINEAR")
-            $xmlWriter.WriteElementString("remediationFunctionBaseEffort", "")
+        $xmlWriter.WriteElementString("name", $rule.CommonName)
+        $xmlWriter.WriteElementString("description", $rule.Description)
+        $xmlWriter.WriteElementString("cardinality", "SINGLE")
+         $xmlWriter.WriteElementString("remediationFunction", "LINEAR")
+		$xmlWriter.WriteElementString("descriptionFormat", "MARKDOWN")
+			
+        $xmlWriter.WriteElementString("remediationFunctionBaseEffort", "")
          $remediationDefaultTime = "2min";
          $severity = "INFO";
         
@@ -56,3 +78,12 @@ $reportFile = Join-path $PSScriptRoot "powershell-rules.xml"
     $xmlWriter.Finalize
     $xmlWriter.Flush
     $xmlWriter.Close();
+
+	
+	   $xmlProfileWriter.WriteEndDocument();
+    
+	
+    # Finish The Document
+    $xmlProfileWriter.Finalize
+    $xmlProfileWriter.Flush
+    $xmlProfileWriter.Close();
